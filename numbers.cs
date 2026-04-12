0a45f925b8ac65045f5e92afc73829692d5f98ff
F3nixTux <F3nixTux@protonmail.com>
2026-04-12 22:53:51 +0300

    Initial commit

--- numbers.cs
+++ numbers.cs
@@ -1,4 +1,3 @@
-// WorkWithIntegers();
 void WorkWithIntegers()
 {
     int a = 18;
@@ -39,6 +38,24 @@ void OrderPrecedence()
     int g = 3;
     int h = (e + f) / g;
     Console.WriteLine(h);
+}
+
+void IntegerPresicionsAndLimits()
+{
+    // Integer quotient and remainder
+    int a = 7;
+    int b = 4;
+    int c = 3;
+    int d = (a + b) / c;
+    int e = (a + b ) % c;
+    Console.WriteLine($"quotient: {d}");
+    Console.WriteLine($"remainder: {e}");
+
+    // Integer limits
+    int max = int.MaxValue;
+    int min = int.MinValue;
+    Console.WriteLine($"The range of integers is {min} to {max}");
+
 }
 
 void IntegersOtherStuff()
