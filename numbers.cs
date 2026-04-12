ce5853bf6d0c925fad1839a28f61304b93bac0f4
F3nix Tux <F3nixTux@protonmail.com>
2026-04-12 16:03:21 +0300

    initial commit

---
+++ numbers.cs
@@ -0,0 +1,16 @@
+int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c);
+
+// Substraction
+c = a - b;
+Console.WriteLine(c);
+
+// Multiplication
+c = a * b;
+Console.WriteLine(c);
+
+// Division
+c = a / b;
+Console.WriteLine(c);
