d7396fcde2f5deaf3693b5109509f5adf6599144
unknown <1113@edu.itstep.lv>
2026-05-08 19:23:06 +0300

    Initial release

---
+++ branches-loops.cs
@@ -0,0 +1,62 @@
+ExploreIf();
+
+void ExploreIf()
+{
+    int a = 5;
+    int b = 3;
+    if (a + b > 10)
+    {
+        Console.WriteLine("The answer is greater than 10");
+    }
+    else
+    {
+        Console.WriteLine("The answer is not greater than 10");
+    }
+
+    int c = 4;
+    if ((a + b + c > 10) && (a > b))
+    {
+        Console.WriteLine("The answer is greater than 10");
+        Console.WriteLine("And the first number is greater than the second");
+    }
+    else
+    {
+        Console.WriteLine("The answer is not greater than 10");
+        Console.WriteLine("Or the first number is not greater than the second");
+    }
+
+    if ((a + b + c > 10) || (a > b))
+    {
+        Console.WriteLine("The answer is greater than 10");
+        Console.WriteLine("Or the first number is greater than the second");
+    }
+    else
+    {
+        Console.WriteLine("The answer is not greater than 10");
+        Console.WriteLine("And the first number is not greater than the second");
+    }
+}
+
+int counter = 0;
+
+do
+{
+    Console.WriteLine($"Hello World! The counter is {counter}");
+    counter++;
+}
+
+while (counter < 10);
+
+for (int counter 0; counter < 10; counter++)
+{
+    Console.WriteLine($"Hello World! The counter is {counter}");
+}
+
+for (int row = 1; row < 11; row++)
+{
+    Console.WriteLine($"The row is {row}");
+    for (char column = 'a'; column < 'k'; column++)
+    {
+        Console.WriteLine($"The column is {column}")
+    }
+}
